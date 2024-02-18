c9209074d5e6ab082c33926e06caa2d82c0a45c2
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-02-18 14:05:48 +0400

    Update code.cs

--- code.cs
+++ code.cs
@@ -60,3 +60,44 @@ class Program
     }
 }
 
+//=============================================================================//
+
+using System;
+
+class Program
+{
+    private static readonly int Number1 = 1;
+    private static readonly int Number2 = 2;
+    private static readonly int Number3 = 3;
+    private static readonly int Number4 = 4;
+    
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Fun_1();
+        program.Fun_2();
+        
+    }
+
+    private void Fun_1()
+    {
+        int[] get1 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] get2 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] get3 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] get4 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        int[] get5 = new int[10] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+        var run1 = get1[1];
+        var run2 = get1[2];
+        var run3 = get1[3];
+        var run4 = get1[4];
+        var run5 = get1[5];
+        
+    }
+
+    private void Fun_2()
+    {
+        
+    }
+}
+
