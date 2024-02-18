fef9eb9d5789c16163a5d6c69a9db796fc7597a2
Ismayil <149945594+ismayilqarayev@users.noreply.github.com>
2024-02-18 11:47:23 +0400

    Update code.cs

--- code.cs
+++ code.cs
@@ -33,3 +33,30 @@ namespace ConsoleApp1
         }
     }
 }
+
+//==============================================================================//
+
+using System;
+
+class Program
+{
+    private static readonly int Number1 = 1;
+    private static readonly int Number2 = 2;
+    private static readonly int Number3 = 3;
+    private static readonly int Number4 = 4;
+    
+    public static void Main(string[] args)
+    {
+        Program program = new Program();
+        program.Fun_1(Number1);
+        
+    }
+
+    private void Fun_1(int num1)
+    {
+        int[] num2 = new int[6] { 1,2,3,4,5,6 };
+        var get = num2[2];
+        Console.WriteLine($"{num1}");
+    }
+}
+
